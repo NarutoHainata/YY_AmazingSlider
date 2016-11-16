cbcbb49c5c41fa86db5fe55bb9684559b88d2e83
populary <410445919@qq.com>
2016-11-16 17:14:25 +0800

    Initial Commit

---
+++ YY_AmazingSlider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YY_AmazingSlider
+//
+//  Created by yang on 2016/11/16.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
